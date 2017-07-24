17d67c1ee58671be7fdfbd6179dca589253eac6f
Geoff Lankow <geoff@darktrojan.net>
2017-07-24 21:57:59 +1200

    Further development of options page

--- webextension/options.js
+++ webextension/options.js
@@ -1,12 +1,36 @@
 /* globals browser */
 let browsersList = document.getElementById('browsers');
 browsersList.onclick = function(event) {
+	let li = event.target;
+	while (li != this && li.localName != 'li') {
+		li = li.parentNode;
+	}
+	if (li == this) {
+		return;
+	}
 	if (event.target.classList.contains('removeBrowser')) {
-		let li = event.target.parentNode.parentNode.parentNode;
 		let id = parseInt(li.dataset.id, 10);
 		browser.runtime.sendMessage({action: 'remove_browser', id}).then(function() {
 			li.remove();
 		});
+		return;
+	}
+	let selected = this.querySelector('.selected');
+	if (selected) {
+		selected.classList.remove('selected');
+	}
+	li.classList.add('selected');
+
+	document.forms.details.name.value = li.querySelector('.name').textContent;
+	document.forms.details.command.value = li.querySelector('.command').textContent;
+	selected = logosList.querySelector('.selected');
+	if (selected) {
+		selected.classList.remove('selected');
+	}
+	for (let l of logosList.children) {
+		if (l.dataset.name == li.dataset.icon) {
+			l.classList.add('selected');
+		}
 	}
 };
 
@@ -34,6 +58,7 @@ browser.runtime.sendMessage({action: 'get_browsers'}).then(function(browsers) {
 function add_browser(b) {
 	let li = browsersTemplate.content.firstElementChild.cloneNode(true);
 	li.dataset.id = b.id;
+	li.dataset.icon = b.icon;
 	li.querySelector('img').src = 'icons/' + b.icon + '_64x64.png';
 	li.querySelector('div.name').textContent = b.name;
 	li.querySelector('div.command').textContent = Array.isArray(b.command) ? b.command.join(' ') : b.command;
@@ -86,3 +111,54 @@ function sort_alphabetically() {
 		)
 	});
 }
+
+// NB: this list isn't quite in order.
+let logos = [
+	'chrome',
+	'chrome-beta',
+	'chrome-dev',
+	'chrome-canary',
+	'chromium',
+	'edge',
+	'firefox_1.5-3',
+	'firefox_3.5-22',
+	'firefox',
+	'firefox-beta',
+	'firefox-nightly',
+	'internet-explorer_6',
+	'internet-explorer_7-8',
+	'internet-explorer_9-11',
+	'opera',
+	'opera-beta',
+	'opera-developer',
+	'pale-moon',
+	'safari',
+	'seamonkey',
+	'vivaldi',
+	'waterfox'
+];
+let logosList = document.getElementById('logos');
+let logosTemplate = logosList.querySelector('template');
+for (let l of logos) {
+	let li = logosTemplate.content.firstElementChild.cloneNode(true);
+	li.dataset.name = l;
+	li.querySelector('img').src = 'icons/' + l + '_32x32.png';
+	li.title = l.replace(/\b-?([a-z])/g, m => m.replace('-', ' ').toUpperCase()).replace('_', ' ');
+	logosList.appendChild(li);
+}
+
+logosList.onclick = function(event) {
+	let target = event.target;
+	while (target != this && target.localName != 'li') {
+		target = target.parentNode;
+	}
+	if (target == this) {
+		return;
+	}
+
+	let selected = this.querySelector('.selected');
+	if (selected) {
+		selected.classList.remove('selected');
+	}
+	target.classList.add('selected');
+};
