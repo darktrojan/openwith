d63d77da671c0b178cb06e13727bd9b0ed87ac75
Geoff Lankow <geoff@darktrojan.net>
2017-11-09 11:01:47 +1300

    Remove webextension from legacy branch

--- webextension/installed.js
+++
@@ -1,8 +0,0 @@
-/* globals chrome, get_strings */
-get_strings();
-
-document.querySelector('button').onclick = function() {
-	chrome.runtime.openOptionsPage(function() {
-		window.close();
-	});
-};
